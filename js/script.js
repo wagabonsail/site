4f44ecea4875d22c338438c641ca5f6bcab3cf63
wagabonsail <pablo.ouint@gmail.com>
2025-07-04 03:24:34 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ const navLinks = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
 });
+// ==== CARROUSEL NAVIRE ===============================================
+const track = document.getElementById('carouselTrack');
+const btnPrev = document.querySelector('.carousel-btn.prev');
+const btnNext = document.querySelector('.carousel-btn.next');
+
+btnPrev.addEventListener('click', () => {
+  track.scrollBy({ left: -track.clientWidth, behavior: 'smooth' });
+});
+btnNext.addEventListener('click', () => {
+  track.scrollBy({ left:  track.clientWidth, behavior: 'smooth' });
+});
